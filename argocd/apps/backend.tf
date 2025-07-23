816f5c0e3d86219d596d4af9534b2badec1faf96
Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>
2025-07-23 11:27:21 -0400

    [func] correction de la key du backend
    Signed-off-by: Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>

--- argocd/apps/backend.tf
+++ argocd/apps/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket               = "x-road-preprod-github-action-ressources"
     workspace_key_prefix = "environments"
-    key                  = "xroad-k8s/terraform.tfstate"
+    key                  = "projects/base/test-argocd/terraform.tfstate"
     region               = "ca-central-1"
     profile              = "xroad-preprod"   
   }
