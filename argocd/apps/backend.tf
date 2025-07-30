f18aca61bd2dd2ba0f0b50c8d44deb98c8881b44
Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>
2025-07-30 09:04:07 -0400

    [func] enleve vars d'environnement non utilisées
    Signed-off-by: Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>

--- argocd/apps/backend.tf
+++ argocd/apps/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket               = "x-road-preprod-github-action-ressources"
     workspace_key_prefix = "environments"
     key                  = "projects/base/test-argocd/terraform.tfstate"
-    region               = "ca-central-1"
-    profile              = "xroad-preprod"   
+    region               = "ca-central-1" 
   }
 }
